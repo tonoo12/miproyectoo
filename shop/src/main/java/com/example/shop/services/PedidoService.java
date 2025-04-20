ef685862fc4d8fed755c0538b4f25df2d94b421c
tonoo12 <joseantoniocavero12@gmail.com>
2025-04-20 18:00:36 -0500

    Update PedidoService.java

--- shop/src/main/java/com/example/shop/services/PedidoService.java
+++ shop/src/main/java/com/example/shop/services/PedidoService.java
@@ -14,31 +14,31 @@ public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
 
-    // Obtener todos los pedidos
+    
     public List<Pedido> listarPedidos() {
         return pedidoRepository.findAll();
     }
 
-    // Obtener pedido por ID
+   
     public Optional<Pedido> obtenerPedidoPorId(Long id) {
         return pedidoRepository.findById(id);
     }
 
-    // Crear un nuevo pedido
+    
     public Pedido crearPedido(Pedido pedido) {
         return pedidoRepository.save(pedido);
     }
 
-    // Actualizar un pedido
+    
     public Pedido actualizarPedido(Long id, Pedido pedidoActualizado) {
         if (pedidoRepository.existsById(id)) {
-            pedidoActualizado.setIdPedido(id); // Establecer el ID del pedido a actualizar
+            pedidoActualizado.setIdPedido(id); 
             return pedidoRepository.save(pedidoActualizado);
         }
-        return null; // En caso de que no exista el pedido
+        return null; 
     }
 
-    // Eliminar un pedido
+   
     public boolean eliminarPedido(Long id) {
         if (pedidoRepository.existsById(id)) {
             pedidoRepository.deleteById(id);
