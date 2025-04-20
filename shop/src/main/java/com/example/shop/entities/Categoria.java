be39def5cfd79d00bb48a315369aba594943e76a
JuanGarcia <juan2525.2004@gmail.com>
2025-04-20 11:34:42 -0500

    feat: creacion de dos entities

---
+++ shop/src/main/java/com/example/shop/entities/Categoria.java
@@ -0,0 +1,24 @@
+package com.example.shop.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private long idCategoria;
+
+    @Column(name = "nombre_categoria")
+    private String nombreCategoria;
+
+}
---
+++ shop/src/main/java/com/example/shop/entities/Producto.java
@@ -0,0 +1,31 @@
+package com.example.shop.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private long idProducto;
+
+    @Column(name = "nombre_producto")
+    private String nombreProducto;
+
+    @Column(name = "descripcion_producto")
+    private String descripcionProducto;
+
+    @Column(name = "precio_producto")
+    private double precioProducto;
+
+    @Column(name = "stock_producto")
+    private int stockProducto;
+}
