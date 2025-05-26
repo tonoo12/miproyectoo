a63bb1c51395582fa6238ef484327b02b2f81fc8
tonoo12 <joseantoniocavero12@gmail.com>
2025-05-25 21:35:16 -0500

    Create contacto.java

---
+++ shop/src/main/java/com/example/shop/entities/contacto.java
@@ -0,0 +1,29 @@
+package com.example.shop.entities;
+
+public class contacto {
+    private String nombre;
+    private String correo;
+    private String mensaje;
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
