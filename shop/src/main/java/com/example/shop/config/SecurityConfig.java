9a4148900a52f5569e757fef7ebdf06a7b95bd97
JuanGarcia <juan2525.2004@gmail.com>
2025-06-20 20:42:25 -0500

    Login y Register con roles terminado

--- shop/src/main/java/com/example/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/example/shop/config/SecurityConfig.java
@@ -1,37 +1,56 @@
 package com.example.shop.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.shop.services.UsuarioService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // Configuracion de seguridad
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/register", "/contactanos/**", "/nosotros", "/login", "/css/**", "/images/**",
+                                "/js/**", "/", "/inicio", "/productos/**")
+                        .permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/register", "/contactanos/**", "/nosotros", "/login", "/css/**", "/images/**","/js/**", "/", "/inicio", "/productos/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/admin", true)
+                        .defaultSuccessUrl("/", true)
                         .permitAll())
-                .logout(logout -> logout.permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .permitAll())
                 .exceptionHandling(ex -> ex.accessDeniedPage("/acceso-denegado"));
         return http.build();
 
