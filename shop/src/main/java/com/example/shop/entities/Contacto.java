78a95c531f158f9800e05c84611ce56aabf8a921
tonoo12 <joseantoniocavero12@gmail.com>
2025-06-29 14:41:05 -0500

    Update Contacto.java

--- shop/src/main/java/com/example/shop/entities/Contacto.java
+++ shop/src/main/java/com/example/shop/entities/Contacto.java
@@ -11,7 +11,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "contactos")
-public class contacto {
+public class Contacto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
