192afdc92c9a9a6d58e472c76bd030fb079d6b2f
tonoo12 <joseantoniocavero12@gmail.com>
2025-06-29 17:39:14 -0500

    Delete shop/src/main/java/com/example/shop/repositories directory

--- shop/src/main/java/com/example/shop/repositories/TallaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shop.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.shop.entities.Talla;
-
-public interface TallaRepository extends JpaRepository<Talla, Long>{
-    
-}
