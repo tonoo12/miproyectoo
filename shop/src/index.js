4a933ca4c8df828322033cdb4561e5b7fb4dc6ab
tonoo12 <joseantoniocavero12@gmail.com>
2025-06-29 20:19:53 -0500

    Update index.js

--- shop/src/index.js
+++ shop/src/index.js
@@ -1,12 +1,17 @@
-// src/index.js
-const http = require('http');
+const express = require('express');
+const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.end('Hola mundo desde Node.js en Docker!');
+// Sirve archivos estáticos si tienes CSS, JS, imágenes, etc.
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Redirige la ruta raíz a tu HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/index.html'));
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
 });
